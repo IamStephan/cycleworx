75a5b307b692d8db52403a77fa5eaafafea9fb36
iamstephan <stephanburger54@gmail.com>
2021-03-19 14:52:06 +0200

    changed outline color

--- tailwind.config.js
+++ tailwind.config.js
@@ -316,7 +316,7 @@ module.exports = {
         30: ".3",
       },
       boxShadow: {
-        outline: "0 0 0 3px rgba(101, 31, 255, 0.4)",
+        outline: "0 0 0 3px rgba(56, 142, 60, 0.75)",
       },
     },
   },
