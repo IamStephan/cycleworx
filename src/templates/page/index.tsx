649ca8fa2695ecd12aad07de75be53133d0ca12d
iamstephan <stephanburger54@gmail.com>
2021-03-19 14:52:46 +0200

    home page transparent nav

--- src/templates/page/index.tsx
+++ src/templates/page/index.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import Navbar from "@components/navbar"
 import Footer from "@components/footer"
 
-interface Props {}
+interface Props {
+  isNavTransparent?: boolean
+}
 
-const PageTemplate: React.FC<Props> = ({ children }) => {
+const PageTemplate: React.FC<Props> = ({ children, isNavTransparent }) => {
   return (
     <>
-      <Navbar />
+      <Navbar isTransparent={isNavTransparent} />
       <main>{children}</main>
       <Footer />
     </>
