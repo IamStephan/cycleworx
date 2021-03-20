32e4659f302c11ec1432778a13c6f212d6b3f957
iamstephan <stephanburger54@gmail.com>
2021-03-20 20:52:22 +0200

    team section

--- src/sections/team/index.tsx
+++ src/sections/team/index.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const TeamSection = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
-          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-green-900 uppercase rounded-full bg-green-accent-400">
             Dream Team
           </p>
         </div>
@@ -14,7 +15,7 @@ const TeamSection = () => {
             <svg
               viewBox="0 0 52 24"
               fill="currentColor"
-              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-green-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
             >
               <defs>
                 <pattern
@@ -33,80 +34,81 @@ const TeamSection = () => {
                 height="24"
               />
             </svg>
-            <span className="relative">Welcome</span>
+            <span className="relative">A</span>
           </span>{" "}
-          our talented team of professionals
+          team of seasoned professionals
         </h2>
         <p className="text-base text-gray-700 md:text-lg">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque rem aperiam, eaque ipsa quae.
+          Our team of professionals have been trained to service you and your
+          bike to the absolute best of standards. We are friendly, industrious
+          and eager to help.
         </p>
       </div>
       <div className="grid gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-3">
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-            alt="Person"
+            src="../../assets/images/homepage/team-1.jpg"
+            alt="Jon Amira"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Oliver Aguilerra</p>
-            <p className="text-sm text-gray-800">Product Manager</p>
+            <p className="text-lg font-bold">Jon Amira</p>
+            <p className="text-sm text-gray-800">Owner | Mechanic</p>
           </div>
         </div>
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            alt="Person"
+            src="../../assets/images/homepage/team-2.jpg"
+            alt="Prince Mazie"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Marta Clermont</p>
-            <p className="text-sm text-gray-800">Design Team Lead</p>
+            <p className="text-lg font-bold">Prince Mazie</p>
+            <p className="text-sm text-gray-800">Mechanic</p>
           </div>
         </div>
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            alt="Person"
+            src="../../assets/images/homepage/team-3.jpg"
+            alt="Kyle Swanepoel"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Anthony Geek</p>
-            <p className="text-sm text-gray-800">CTO, Lorem Inc.</p>
+            <p className="text-lg font-bold">Kyle Swanepoel</p>
+            <p className="text-sm text-gray-800">Mechanic</p>
           </div>
         </div>
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            alt="Person"
+            src="../../assets/images/homepage/team-4.jpg"
+            alt="Stephan Burger"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Alice Melbourne</p>
-            <p className="text-sm text-gray-800">Human Resources</p>
+            <p className="text-lg font-bold">Stephan</p>
+            <p className="text-sm text-gray-800">Mechanic</p>
           </div>
         </div>
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/3785077/pexels-photo-3785077.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-            alt="Person"
+            src="../../assets/images/homepage/team-5.jpg"
+            alt="Tiaan"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Martin Garix</p>
-            <p className="text-sm text-gray-800">Bad boy</p>
+            <p className="text-lg font-bold">Tiaan</p>
+            <p className="text-sm text-gray-800">Mechanic</p>
           </div>
         </div>
         <div className="flex">
-          <img
+          <StaticImage
             className="object-cover w-20 h-20 mr-4 rounded-full shadow"
-            src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-            alt="Person"
+            src="../../assets/images/homepage/team-6.jpg"
+            alt="Ryan"
           />
           <div className="flex flex-col justify-center">
-            <p className="text-lg font-bold">Andrew Larkin</p>
-            <p className="text-sm text-gray-800">Backend Developer</p>
+            <p className="text-lg font-bold">Ryan</p>
+            <p className="text-sm text-gray-800">Mechanic</p>
           </div>
         </div>
       </div>
