3ed1ae3a78deeedc6cba8ec7b6e548826a771616
iamstephan <stephanburger54@gmail.com>
2021-03-21 11:09:44 +0200

    title change

--- src/sections/about/index.tsx
+++ src/sections/about/index.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     <section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-          A quick guide to our{" "}
+          An overview of our{" "}
           <span className="inline-block text-green-600">journey</span>
         </h2>
         <p className="text-base text-gray-700 md:text-lg">
