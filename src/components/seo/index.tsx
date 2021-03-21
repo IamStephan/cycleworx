7e592bd15a94305972098d9200b9f3c92a2f76ae
iamstephan <stephanburger54@gmail.com>
2021-03-21 22:17:07 +0200

    removed redundent options

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -6,10 +6,9 @@ import { useStaticQuery, graphql } from "gatsby"
 interface Props {
   title: string
   description: string
-  image: string
 }
 
-const SEO: React.FC<Props> = ({ title, description, image }) => {
+const SEO: React.FC<Props> = ({ title, description }) => {
   const { pathname } = useLocation()
   const { site } = useStaticQuery(query)
 
@@ -18,22 +17,17 @@ const SEO: React.FC<Props> = ({ title, description, image }) => {
     titleTemplate,
     defaultDescription,
     siteUrl,
-    defaultImage,
-    twitterUsername,
   } = site.siteMetadata
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
     url: `${siteUrl}${pathname}`,
   }
 
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
-
-      <meta name="image" content={seo.image} />
 
       {seo.url && <meta property="og:url" content={seo.url} />}
 
@@ -42,48 +36,20 @@ const SEO: React.FC<Props> = ({ title, description, image }) => {
       {seo.description && (
         <meta property="og:description" content={seo.description} />
       )}
-
-      {seo.image && <meta property="og:image" content={seo.image} />}
-
-      <meta name="twitter:card" content="summary_large_image" />
-
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
-
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
-
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   )
 }
 
 export default SEO
 
-// const query = graphql`
-//   query SEO {
-//     site {
-//       siteMetadata {
-//         defaultTitle: title
-//         titleTemplate
-//         defaultDescription: description
-//         siteUrl: url
-//         defaultImage: image
-//         twitterUsername
-//       }
-//     }
-//   }
-// `
-
 const query = graphql`
   query SEO {
     site {
       siteMetadata {
         defaultTitle: title
+        titleTemplate
+        defaultDescription: description
+        siteUrl: url
       }
     }
   }
