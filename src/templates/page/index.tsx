a409a8e63f3c6fc3cf1ca00e26afca10143162eb
iamstephan <stephanburger54@gmail.com>
2021-03-21 22:17:47 +0200

    added page options and seo component

--- src/templates/page/index.tsx
+++ src/templates/page/index.tsx
@@ -1,14 +1,23 @@
 import React from "react"
 import Navbar from "@components/navbar"
 import Footer from "@components/footer"
+import SEO from "@components/seo"
 
 interface Props {
+  title: string
+  description: string
   isNavTransparent?: boolean
 }
 
-const PageTemplate: React.FC<Props> = ({ children, isNavTransparent }) => {
+const PageTemplate: React.FC<Props> = ({
+  title,
+  description,
+  isNavTransparent,
+  children,
+}) => {
   return (
     <>
+      <SEO title={title} description={description} />
       <Navbar isTransparent={isNavTransparent} />
       <main>{children}</main>
       <Footer />
